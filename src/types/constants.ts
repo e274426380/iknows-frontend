ff3dad0ce5268c6becc003892fc05d663e4c5050
e274426380 <867690443@qq.com>
2023-01-13 13:23:59 +0800

    Improve:根据dev网络修改合约地址

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,6 +1,6 @@
-export const contractAddress = '0x8b131dae4fac6d3a9ca1d32f8310781c1457ca5f';
-export const userRegisterAddress = '0x7acafa2d93fd96f64b430a55ce72f354a1634d0b'; //记录用户是否重复注册的合约地址
-export const topicBoardAddress = '0x049b94bad6bce1a7f8ae98b5b3fb539605e04b73'; //记录发贴数据
+export const contractAddress = '0x84142b7d621f844f405a9a8a8641e645185d4d4a';
+export const userRegisterAddress = '0x760fff36b84f2788006d0c02569d492dcdd24f76'; //记录用户是否重复注册的合约地址
+export const topicBoardAddress = '0xcb3d20404f1119f76104debbb838af323641cc15'; //记录发贴数据
 export const suiRpcUrl = 'https://fullnode.devnet.sui.io:443'
 export const localStorageKeys = {
     address: 'user_sui_address',
